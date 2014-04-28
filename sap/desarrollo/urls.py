2dd4347bc36224229777a9df6ebca65f92960e02
GustavoCabral <andrescabral.c@gmail.com>
2014-04-28 17:11:18 -0400

    Agregado: fases de proyecto en el módulo de desarrollo.

--- sap/desarrollo/urls.py
+++ sap/desarrollo/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from desarrollo.views import desarrollo_view
+from desarrollo.views import fases_proyecto_view
 
 urlpatterns = patterns('',
     url(r'^desarrollo/$', desarrollo_view, name="vista_desarrollo"),
+    url(r'^desarrollo/fases/proyecto/(?P<id_proyecto>.*)/$', fases_proyecto_view, name="vista_desarrollo_fases_proyecto"),
 )
--- sap/desarrollo/views.py
+++ sap/desarrollo/views.py
@@ -10,3 +10,12 @@ def desarrollo_view(request):
     proyectos = Proyecto.objects.filter(estado=1)
     ctx = {'proyectos': proyectos}
     return render_to_response('desarrollo.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+@permiso_requerido(permiso="Gestionar fases de proyecto")
+def fases_proyecto_view(request, id_proyecto):
+    
+    proyecto = Proyecto.objects.get(id=id_proyecto)
+    fases = proyecto.fases.all()
+    ctx = {'proyecto':proyecto, 'fases':fases}
+    return render_to_response('fases_proyecto.html', ctx, context_instance=RequestContext(request))
