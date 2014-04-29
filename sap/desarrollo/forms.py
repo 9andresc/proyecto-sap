6d36de62878895f3fed18b5df0e1086d56eb8666
GustavoCabral <andrescabral.c@gmail.com>
2014-04-29 19:20:19 -0400

    Agregados: modificar item y visualizar item.

--- sap/desarrollo/forms.py
+++ sap/desarrollo/forms.py
@@ -20,3 +20,24 @@ class CrearItemForm(forms.Form):
             return costo
         else:
             raise forms.ValidationError('El valor del costo debe ser igual o mayor a cero.')
+        
+class ModificarItemForm(forms.Form):
+
+    nombre = forms.CharField(label="Nombre de item", required=True)
+    descripcion = forms.CharField(label="Descripcion", required=False)
+    complejidad = forms.IntegerField(label="Complejidad", required=True)
+    costo = forms.FloatField(label="Costo", required=True)
+    
+    def clean_complejidad(self):
+        complejidad = self.cleaned_data['complejidad']
+        if complejidad > 0 and complejidad <= 10:
+            return complejidad
+        else:
+            raise forms.ValidationError('El valor de la complejidad debe estar en el rango [1, 10].')
+        
+    def clean_costo(self):
+        costo = self.cleaned_data['costo']
+        if costo >= 0:
+            return costo
+        else:
+            raise forms.ValidationError('El valor del costo debe ser igual o mayor a cero.')
--- sap/desarrollo/views.py
+++ sap/desarrollo/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from administracion.models import Proyecto, Fase
 from desarrollo.models import Item
-from desarrollo.forms import CrearItemForm
+from desarrollo.forms import CrearItemForm, ModificarItemForm
 from inicio.decorators import permiso_requerido
 
 @login_required(login_url='/login/')
@@ -59,3 +59,46 @@ def crear_item_view(request, id_fase, id_proyecto):
             return render_to_response('crear_item.html', ctx, context_instance=RequestContext(request))
     ctx = {'form':form, 'fase':fase, 'proyecto':proyecto}
     return render_to_response('crear_item.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+@permiso_requerido(permiso="Modificar item")
+def modificar_item_view(request, id_item, id_fase, id_proyecto):
+    
+    item = Item.objects.get(id=id_item[0])
+    fase = Fase.objects.get(id=id_fase[0])
+    proyecto = Proyecto.objects.get(id=id_proyecto[0])
+    form = ModificarItemForm()
+    if request.method == "POST":
+        form = ModificarItemForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data['nombre']
+            descripcion = form.cleaned_data['descripcion']
+            complejidad = form.cleaned_data['complejidad']
+            costo = form.cleaned_data['costo']
+            
+            item.nombre = nombre
+            item.descripcion = descripcion
+            item.complejidad = complejidad
+            item.costo = costo
+            item.save()
+            return HttpResponseRedirect('/desarrollo/items/item/%s/fase/%s/proyecto/%s/'%(item.id, fase.id, proyecto.id))
+    
+    if request.method == "GET":
+        form = ModificarItemForm(initial={
+            'nombre': item.nombre,
+            'descripcion': item.descripcion,
+            'costo': item.costo,
+            'complejidad': item.complejidad,
+            })
+    ctx = {'form':form, 'item':item, 'fase':fase, 'proyecto':proyecto}
+    return render_to_response('modificar_item.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+@permiso_requerido(permiso="Visualizar item")
+def visualizar_item_view(request, id_item, id_fase, id_proyecto):
+
+    item = Item.objects.get(id=id_item[0])
+    fase = Fase.objects.get(id=id_fase[0])
+    proyecto = Proyecto.objects.get(id=id_proyecto[0])
+    ctx = {'item':item, 'fase': fase, 'proyecto':proyecto}
+    return render_to_response('visualizar_item.html', ctx, context_instance=RequestContext(request))
