63e2672da3ce7ebf5a854bfefe379b6245b2451d
GustavoCabral <andrescabral.c@gmail.com>
2014-04-29 18:28:34 -0400

    Agregado: crear item.

--- sap/desarrollo/urls.py
+++ sap/desarrollo/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, url
 from desarrollo.views import desarrollo_view
 from desarrollo.views import fases_proyecto_view
 from desarrollo.views import items_fase_view
+from desarrollo.views import crear_item_view
 
 urlpatterns = patterns('',
     url(r'^desarrollo/$', desarrollo_view, name="vista_desarrollo"),
     url(r'^desarrollo/fases/proyecto/(?P<id_proyecto>.*)/$', fases_proyecto_view, name="vista_desarrollo_fases_proyecto"),
     url(r'^desarrollo/items/fase/(?P<id_fase>.*)/proyecto/(?P<id_proyecto>.*)/$', items_fase_view, name="vista_desarrollo_items_fase"),
+    url(r'^desarrollo/items/crear_item/fase/(?P<id_fase>.*)/proyecto/(?P<id_proyecto>.*)/$', crear_item_view, name="vista_desarrollo_crear_item"),
 )
--- sap/desarrollo/views.py
+++ sap/desarrollo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render_to_response
+from django.http.response import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from administracion.models import Proyecto, Fase
+from desarrollo.models import Item
+from desarrollo.forms import CrearItemForm
 from inicio.decorators import permiso_requerido
 
 @login_required(login_url='/login/')
@@ -29,3 +32,30 @@ def items_fase_view(request, id_fase, id_proyecto):
     items = fase.items.all()
     ctx = {'proyecto':proyecto, 'fase':fase, 'items':items}
     return render_to_response('items_fase.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+@permiso_requerido(permiso="Crear item")
+def crear_item_view(request, id_fase, id_proyecto):
+    
+    fase = Fase.objects.get(id=id_fase[0])
+    proyecto = Proyecto.objects.get(id=id_proyecto[0])
+    form = CrearItemForm()
+    if request.method == "POST":
+        form = CrearItemForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data['nombre']
+            descripcion = form.cleaned_data['descripcion']
+            complejidad = form.cleaned_data['complejidad']
+            costo = form.cleaned_data['costo']
+            
+            item = Item.objects.create(nombre=nombre, descripcion=descripcion, complejidad=complejidad, costo=costo)
+            item.save()
+            fase.items.add(item)
+            fase.save()
+            return HttpResponseRedirect('/desarrollo/items/fase/%s/proyecto/%s/'%(fase.id, proyecto.id))
+            
+        else:
+            ctx = {'form':form, 'fase':fase, 'proyecto':proyecto}
+            return render_to_response('crear_item.html', ctx, context_instance=RequestContext(request))
+    ctx = {'form':form, 'fase':fase, 'proyecto':proyecto}
+    return render_to_response('crear_item.html', ctx, context_instance=RequestContext(request))
