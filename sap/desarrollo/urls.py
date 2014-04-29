7f8efc3c3946856d385f302d4c3508e21c3f5c6e
GustavoCabral <andrescabral.c@gmail.com>
2014-04-29 00:52:46 -0400

    Cambios en el módulo de desarrollo Se ha agregado la gestión de items por fase, además se han modificado algunos aspectos en los templates de desarrollo.

--- sap/desarrollo/urls.py
+++ sap/desarrollo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 from desarrollo.views import desarrollo_view
 from desarrollo.views import fases_proyecto_view
+from desarrollo.views import items_fase_view
 
 urlpatterns = patterns('',
     url(r'^desarrollo/$', desarrollo_view, name="vista_desarrollo"),
     url(r'^desarrollo/fases/proyecto/(?P<id_proyecto>.*)/$', fases_proyecto_view, name="vista_desarrollo_fases_proyecto"),
+    url(r'^desarrollo/items/fase/(?P<id_fase>.*)/proyecto/(?P<id_proyecto>.*)/$', items_fase_view, name="vista_desarrollo_items_fase"),
 )
--- sap/desarrollo/views.py
+++ sap/desarrollo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from administracion.models import Proyecto
+from administracion.models import Proyecto, Fase
 from inicio.decorators import permiso_requerido
 
 @login_required(login_url='/login/')
@@ -19,3 +19,13 @@ def fases_proyecto_view(request, id_proyecto):
     fases = proyecto.fases.all()
     ctx = {'proyecto':proyecto, 'fases':fases}
     return render_to_response('fases_proyecto.html', ctx, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+@permiso_requerido(permiso="Gestionar items de fase")
+def items_fase_view(request, id_fase, id_proyecto):
+    
+    proyecto = Proyecto.objects.get(id=id_proyecto)
+    fase = Fase.objects.get(id=id_fase)
+    items = fase.items.all()
+    ctx = {'proyecto':proyecto, 'fase':fase, 'items':items}
+    return render_to_response('items_fase.html', ctx, context_instance=RequestContext(request))
