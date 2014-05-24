c4e8ddb7ca76e47832e68026168b1b76570ad948
GustavoCabral <andrescabral.c@gmail.com>
2014-05-23 23:44:44 -0400

    Las clases LineaBase y SolicitudCambio fueron agregadas al admin del proyecto.

--- sap/desarrollo/admin.py
+++ sap/desarrollo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from desarrollo.models import Item, Fase, TipoItem
+from desarrollo.models import Item, Fase, TipoItem, LineaBase, SolicitudCambio
 
 admin.site.register(Item)
 admin.site.register(Fase)
 admin.site.register(TipoItem)
+admin.site.register(LineaBase)
+admin.site.register(SolicitudCambio)
